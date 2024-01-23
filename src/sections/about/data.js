0b0988af8632a287adb88da118a72d1ac141dd49
Antony Dhan Orejas <orejasantonydhan50@gmail.com>
2024-01-23 13:50:24 +0800

    fix some designs

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -2,7 +2,7 @@ import {FaAward} from 'react-icons/fa';
 import {TbBooks} from 'react-icons/tb';
 
 const data = [
-    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '3+ yrs of experience (School)'},
+    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '3 years web development experience'},
     {id: 2, icon: <TbBooks/>, title: 'Projects', desc: '20+ Completed'}
 ]
 
